d2f52e28bf5c08595e23383c6c7dc95d6f267ffd
HectorSchreiner <hectorschousbo@gmail.com>
2022-08-11 22:58:35 +0200

    cool implements

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use minifb::{Key, Window, WindowOptions};
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
-pub const WHITE: u32 = 0x00000000;
-pub const BLACK: u32 = 0xffffffff;
+#[derive(Clone, Copy)]
+enum Color {
+    WHITE = 0xffffff,
+    BLACK = 0x000000,
+    RED = 0xff0000,
+    GREEN = 0x00ff00,
+    BLUE = 0x0000ff,
+}
 
 fn main() {
     let mut renderer: Renderer = Renderer {
@@ -22,26 +28,15 @@ fn main() {
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    let map: [i32; 64] = [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1,
-    ];
-
-    let pos: Position = Position { x: 20, y: 20 };
-    let square: Square = Square {
-        lenght: 50,
-        height: 50,
-        position: pos,
-    };
+    let square_1 = Square::new(10, 40, [50, 50]);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
             .update_with_buffer(&renderer.buffer, WIDTH, HEIGHT)
             .unwrap();
 
-        renderer.background(WHITE);
-        renderer.rect(&square, BLACK);
+        renderer.background(Color::BLACK);
+        renderer.rect(&square_1, Color::RED);
     }
 }
 
@@ -49,40 +44,68 @@ struct Position {
     x: u32,
     y: u32,
 }
+impl From<(u32, u32)> for Position {
+    fn from(position: (u32, u32)) -> Self {
+        Self {
+            x: position.0,
+            y: position.1,
+        }
+    }
+}
+
+impl From<[u32; 2]> for Position {
+    fn from(position: [u32; 2]) -> Self {
+        Self {
+            x: position[0],
+            y: position[1],
+        }
+    }
+}
+
 struct Square {
     lenght: u32,
     height: u32,
     position: Position,
+}
+
+impl Square {
+    fn new(lenght: u32, height: u32, position: impl Into<Position>) -> Square {
+        Square {
+            lenght,
+            height,
+            position: position.into(),
+        }
+    }
 }
 struct Renderer {
     buffer: Vec<u32>,
 }
 
 impl Renderer {
-    fn draw_pixel(&mut self, position: Position, color: u32, default_color: u32) {
+    fn draw_pixel(&mut self, position: Position, color: Color, default_color: Color) {
         for iter in 0..HEIGHT * WIDTH {
             if self.buffer[iter] == position.x + position.y * HEIGHT as u32 {
-                self.buffer[iter] = color;
+                self.buffer[iter] = color as _;
             } else {
-                self.buffer[iter] = default_color;
+                self.buffer[iter] = default_color as _;
             }
         }
     }
 
-    fn rect(&mut self, square: &Square, color: u32) {
+    fn rect(&mut self, square: &Square, color: Color) {
         let pos_y = square.position.y;
         let pos_x = square.position.x;
 
         for y in pos_y..square.height + pos_y {
             for x in pos_x..square.lenght + pos_x {
-                self.buffer[(y * WIDTH as u32 + x) as usize] = color;
+                self.buffer[(y * WIDTH as u32 + x) as usize] = color as _;
             }
         }
     }
 
-    fn background(&mut self, color: u32) {
+    fn background(&mut self, color: Color) {
         for iter in 0..HEIGHT * WIDTH {
-            self.buffer[iter] = color;
+            self.buffer[iter] = color as _;
         }
     }
 }
